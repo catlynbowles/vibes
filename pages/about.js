2d080ca995f996a0bc8ade978f29ace0336d6bac
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-07 19:53:53 -0600

    yea

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from '@/components/About/About';
+import './globals.css'
 
 export default function Info() {
   return <About />;
--- pages/feeling/[name].js
+++ pages/feeling/[name].js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CheckIn from '@/components/CheckIn/CheckIn'
 import Header from '@/components/Header/Header'
+import '../globals.css'
 
 export default function Name() {
   return (
